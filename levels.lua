785e31dcf4a93efe3be531c4986fb358f655a697
Daniel A. Nagy <nagy.da@gmail.com>
2026-04-16 06:00:25 +0200

    Baldvin's levels

--- levels.lua
+++ levels.lua
@@ -2,31 +2,85 @@
 
 -- Each level is an array of strings with attributes.
 
-corridor = {
-  "######",
-  "#N  *#",
-  "######",
-  controls = editor,
+-- Level 1: straight line, 2-3 moves
+intro = {
+  "####",
+  "#* #",
+  "#  #",
+  "#N #",
+  "####",
+  controls = keys,
   progression = celebrate,
   legend = LEGEND_FULL,
   grid = true
 }
 
-sokoban = {
-  "  ###   ",
-  "  #G#   ",
-  "  # ####",
-  "###B BG#",
-  "#G BN###",
-  "####B#  ",
-  "   #G#  ",
-  "   ###  ",
-  controls = keys,
+-- Level 2: one turn
+one_turn = {
+  "#####",
+  "#  *#",
+  "#   #",
+  "#N  #",
+  "#####",
+  legend = LEGEND_FULL,
+  grid = false
+}
+
+-- Level 3: two turns, longer path
+two_turns = {
+  "#####",
+  "#*  #",
+  "# # #",
+  "#   #",
+  "# # #",
+  "#  N#",
+  "#####",
+  legend = LEGEND_FULL
+}
+
+two_turns2 = {
+  "#####",
+  "#*  #",
+  "# # #",
+  "#   #",
+  "# # #",
+  "#  N#",
+  "#####",
+  controls = editor,
   progression = portal,
   legend = LEGEND_FULL
 }
 
+-- Level 4: moderate complexity
+moderate_complexity = {
+  "######",
+  "#*## #",
+  "#    #",
+  "# ## #",
+  "#    #",
+  "#N   #",
+  "######",
+  legend = LEGEND_FULL
+}
+
+-- Level 5: longer path with dead ends
+dead_ends = {
+  "######",
+  "# # *#",
+  "#    #",
+  "## # #",
+  "#    #",
+  "#N## #",
+  "######",
+  progression = continue,
+  legend = LEGEND_FULL
+}
+
 levels = {
-  corridor,
-  sokoban
+  intro,
+  one_turn,
+  two_turns,
+  two_turns2,
+  moderate_complexity,
+  dead_ends
 }
